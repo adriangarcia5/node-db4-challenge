e58c6a143bc9dcddb624d8da441c221d81c4c540
Adrian-Garcia <agarc5@hotmail.com>
2019-10-10 17:43:06 -0500

    created table structure

---
+++ migrations/20191010174113_create-table-recipes.js
@@ -0,0 +1,40 @@
+exports.up = function(knex) {
+    return knex.schema
+      .createTable("recipe", tbl => {
+        tbl.increments();
+        tbl
+          .string("recipe", 128)
+          .notNullable()
+          .unique();
+      })
+      .createTable("ingredients", tbl => {
+        tbl.increments();
+        tbl
+          .string("ingredient", 128)
+          .notNullable()
+          .unique();
+      })
+      .createTable("final", tbl => {
+        tbl.increments();
+        tbl.integer("quantity").notNullable();
+        tbl
+          .integer("ingredient_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("ingredients");
+        tbl
+          .integer("recipe_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("recipe");
+      });
+  };
+  
+  exports.down = function(knex) {
+    return knex.scheme
+      .dropTableIfExists("final")
+      .dropTableIfExists("ingredients")
+      .dropTableIfExists("recipe");
+  };
